a6ceca8c2b6542c14e685da641510a089b6a856f
Sangeethasan7 <120364555+Sangeethasan7@users.noreply.github.com>
2023-10-11 11:56:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,45 @@
-//complete this code
-class Person {}
+class Person {
+  constructor(name, age) {
+    this._name = name;
+    this._age = age;
+  }
 
-class Student extends Person {}
+  get name() {
+    return this._name;
+  }
 
-class Teacher extends Person {}
+  set age(newAge) {
+    if (newAge >= 0) {
+      this._age = newAge;
+    } else {
+      console.log('Age cannot be negative.');
+    }
+  }
+}
 
-// Do not change the code below this line
-window.Person = Person;
-window.Student = Student;
-window.Teacher = Teacher;
+class Student extends Person {
+  study() {
+    console.log(`${this.name} is studying.`);
+  }
+}
+
+class Teacher extends Person {
+  teach() {
+    console.log(`${this.name} is teaching.`);
+  }
+}
+
+// Create a student and a teacher
+const student = new Student('Alice', 20);
+const teacher = new Teacher('Mr. Smith', 35);
+
+console.log(student.name); // Accessing the name getter
+student.study(); // Calling the study method
+
+console.log(teacher.name); // Accessing the name getter
+teacher.teach(); // Calling the teach method
+
+// Example of setting age using the setter
+student.age = 21; // This will set the age to 21
+teacher.age = -5; // This will log "Age cannot be negative."
+
